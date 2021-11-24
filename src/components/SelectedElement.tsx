760f56184d9f05424107bfd9c6d3d7e89bb8d8d1
FireAndIceFrog <mblawrence00@gmail.com>
2021-11-24 23:18:55 +1300

    got the cool colours changing!

--- src/components/SelectedElement.tsx
+++ src/components/SelectedElement.tsx
@@ -1,28 +1,33 @@
+import { useRef, useState } from "react";
 import { useAppSelector } from "../app/hooks";
 import { intersectedObject } from "../ThreeJs/main";
 
 export default function Selected3DElement() {
     const elmSelected = useAppSelector(state=>state.canvasSlice.elementHasBeenSelected)
-    
 
-    if(elmSelected && intersectedObject){
-        const colour = (intersectedObject as any)?.material?.color as undefined | null | { r:number, b:number, g: number, set: (color: number) => void };
+    const redRef = useRef<HTMLInputElement>(null);
+    const greenRef = useRef<HTMLInputElement>(null);
+    const blueRef = useRef<HTMLInputElement>(null);
+
+    if(elmSelected && intersectedObject && (intersectedObject as any)?.material?.color){
+        const colour = (intersectedObject as any)?.material?.color as  { r:number, b:number, g: number, set: (color: number) => void };
         
-        
-        (intersectedObject as any)?.material?.color?.set(0x00ff00);
-        const onRedChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-            if(colour && colour.set){
-                const currentColor = colour.r + colour.g + colour.b;
-                colour.set(parseInt(e.target.value, 16));
+        const onSubmit = () => {
+            if(redRef.current && greenRef.current && blueRef.current){
+                const red = parseInt(redRef.current.value);
+                const green = parseInt(greenRef.current.value);
+                const blue = parseInt(blueRef.current.value);
+                (intersectedObject as any)?.material?.color?.set(red*0xff0000 + green*0x00ff00 + blue*0x0000ff);
             }
         }
 
         return (
         <div>
             <p>Selected Element: {intersectedObject?.name}</p>
-            <p>Red: <input type="number" value = {colour?.r}></input></p>
-            <p>Green: <input type="number" value = {colour?.g}></input></p>
-            <p>Blue: <input type="number" value = {colour?.b}></input></p>
+            <p>Red: <input type="text" defaultValue = {colour.r*255}  ref = {redRef}></input></p>
+            <p>Green: <input type="text" defaultValue = {colour.g*255} ref = {greenRef}></input></p>
+            <p>Blue: <input type="text" defaultValue = {colour.b*255}  ref = {blueRef }></input></p>
+            <button onClick={onSubmit}>Submit</button>
         </div>
         )
 
